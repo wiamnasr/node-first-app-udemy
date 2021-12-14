46dc35cb8bfa15c99544e87d99c84d8a1e3ed200
Wiam <wiamnasr@gmail.com>
2021-12-14 12:12:52 +0000

    notes on routing order and dynamic segments

--- routes/shop.js
+++ routes/shop.js
@@ -18,6 +18,16 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
+/*
+    The colon ":" signals to express that it should not look for a route, but instead this part (productId) can be anything
+    Express will simply route or load this route for this path
+    We are then able to extract that information through that name (productId in this case)
+    important to note the order, for instance if we had a "router.get("/products/delete")" for instance, if we place it after this one, we'll never reach that route
+    This is because express.js would've already fired this route "/products/:productId". "/delete" will be treated as a dynamic segment such a case
+    In short, if we had a dynamic segment, and a specific route, the specific route has to go first
+*/
+router.get("/products/:productId");
+
 router.get("/cart", shopController.getCart);
 
 router.get("/checkout", shopController.getCheckout);
