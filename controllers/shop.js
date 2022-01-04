ce004e3445f6c6d2d1c6127f4469ff4c31104140
Wiam Nasr <wiamnasr@gmail.com>
2022-01-04 15:16:31 +0000

    viewing product details using findByPk() in sequelize

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,10 +20,10 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   // Express.js automatically gives us that param object on our request
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
