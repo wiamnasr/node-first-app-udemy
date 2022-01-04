20318ff56e27fa01977f56fd13542ca853c8ee87
Wiam Nasr <wiamnasr@gmail.com>
2022-01-04 14:56:25 +0000

    adding new products to the database using sequelize

--- app.js
+++ app.js
@@ -8,15 +8,10 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 
 // importing pool
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
-/*
-  ejs view engine, like pug auto registers itself with Express
-  Has a nice mixture of the extended functionalities of pug (JS that we can use in our templates) & still it uses normal html like handlebars
-  - ejs does not support layouts, but we will find a solution to have some kind of reusability of certain building blocks
-*/
 // the ejs view engine way
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -27,39 +22,8 @@ const adminRoutes = require("./routes/admin");
 // Importing shop routes
 const shopRoutes = require("./routes/shop");
 
-// testing code for db
-// db.execute("SELECT * FROM products")
-//   .then((result) => {
-//     console.log(result[0], result[1]);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-/*
-  Using pug template-ing engine, Setting a global configuration value
-  app.set() allows us to set any values globally on our express application
-  These can be read from the app object with app.get (a way of sharing data across our application, but not something we are interested in doing at the moment here)
-  What we can do, is use a couple of reserved key-names (configuration items that we can set), that do lead to express.js behaving differently
-  Interesting for us from those items is view engine and the views key
-  View engine allows us to tell express for any dynamic template we are trying to render (there will be a special function for doing that), use this engine we are registering here
-  views, allows us to tell express where to find these dynamic views
-
-  - pug template-ing engine ships with built in express support and auto-registers itself with express (this is not the case with all engines)
-  - We can set an additional setting (the views) to tell express where to find our views, however the default setting here for views (express app.set() documentation) is process.cwd() + '/views' (basically our main directory then the views folder)
-  - Putting it here just in case the folder name was not views as the default and we had to set some configuration to tell express where to find our views
-*/
-// app.set("view engine", "pug");
-// Even though its not needed here, but we are including (views is the default
-// app.set("views", "views");
-
-// Body parser middleware
-// extended is a config that must be added. It telling if it should be able to parse non default features (added to comply with what we should use here)
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Serving files statically
-// static method is a built in middleware that serves static files
-// A path to the folder which we want to serve statically is passed in (A folder that we want to grant read access to)
 app.use(express.static(path.join(__dirname, "public")));
 
 // admin Routes with /admin filter
@@ -71,4 +35,14 @@ app.use(shopRoutes);
 // Adding a 404 Error Page (catch-all)
 app.use(errorController.get404);
 
-app.listen(3000);
+// the sync method has a look at all the models defined and then creates appropriate tables and relations (if present) for them
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    // starting the server if we get into this stage
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- controllers/admin.js
+++ controllers/admin.js
@@ -14,60 +14,30 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  // Creating a new object based on the Product class blueprint
-  // This takes the title we have on our input which is submitted
-  const product = new Product(null, title, imageUrl, description, price);
-
-  // Saving the new Product
-  product
-    .save()
-    .then(() => {
-      // redirecting once the insert is completed
-      res.redirect("/");
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((result) => {
+      // console.log(result);
+      console.log("created product...");
     })
     .catch((err) => {
       console.log(err);
     });
-
-  // Later on we want to add more fields here, and therefore will create a new object (makes it clearer whats happening)
-  // otherwise I could've pushed the whole req.body as it has the same structure
-  // products.push({ title: req.body.title });
 };
 
 // Here I plan on passing in my product information
 exports.getEditProduct = (req, res, next) => {
-  /*
-      passing in an additional information field (a boolean set to true)
-      we can check this with an if condition to see if on clicking the save button, we should add the product and send a request to that route, or try to edit it and sed it to a different route
-      Lets say we want to get additional confirmation, by ensuring that people have to pass us a so called query parameter in the url
-      A query parameter can be added to any url by adding a "?" question mark, then a key-value pair, separated by an equal sign
-      We can have multiple query parameters, separated by ampersands "&"
-
-      checking if editMode is set, by accessing query object (created and managed by express)
-      We can try to access the data here, by trying to access the keys we get in our query parameters 
-
-      For the example here, we might check for "edit" key in our query parameters as we are doing below in editMode const
-      e.g.: "localhost:3000/admin/edit-product/12345?edit=true&title=new"
-    */
   const editMode = req.query.edit;
   if (!editMode) {
     return res.redirect("/");
   }
 
-  /*
-    Fetching the product before we pre-populate the form with the product data
-    In edit mode, after not being redirected, we want to get our product info
-    for that we need the product model, which we already have, as well as the product id
-    product id can be retrieved from the incoming request (if we check the routes, we have productId dynamic segment, by this name, we can extract the product id)
-  */
   const prodId = req.params.productId;
 
-  /*
-    Now we can use our product model, and find this product by id
-    then we have a callback where we receive the product which was retrieved
-    A check will be added to see if we have a product, and re-direct the user if not
-    Assuming we get a product, we want to render the page below
-  */
   Product.findById(prodId, (product) => {
     if (!product) {
       // Not the best user experience, usually we want to show an error instead
