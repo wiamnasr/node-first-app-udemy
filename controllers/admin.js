99d1e25e24e2baa2d66183e094d35bf3a76fd602
Wiam Nasr <wiamnasr@gmail.com>
2022-01-10 13:35:50 +0000

    admin controller fix

--- controllers/admin.js
+++ controllers/admin.js
@@ -91,6 +91,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      console.log("Destroyed product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
