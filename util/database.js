87a01d191cfdf5821ce9de929178a9eb3d2f7d60
Wiam Nasr <wiamnasr@gmail.com>
2022-02-10 09:19:51 +0000

    handled security vulnerabilities

--- util/database.js
+++ util/database.js
@@ -1,11 +1,18 @@
 const Sequelize = require("sequelize");
 
+require("dotenv").config();
+
 // the sequelize constructor function needs some options, db name, username and password
 // an optional fourth argument can be added, an options object, in that object, dialect infers that we are connecting to a mysql database here. the host uses localhost by default, but we are explicitly defining it here
-const sequelize = new Sequelize("node-complete", "wiam", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const sequelize = new Sequelize(
+  "node-complete",
+  `${process.env.USERNAME}`,
+  `${process.env.PASSWORD}`,
+  {
+    dialect: "mysql",
+    host: "localhost",
+  }
+);
 
 // exporting the data connection pool managed by sequelize, giving us a lot of useful features
 module.exports = sequelize;
